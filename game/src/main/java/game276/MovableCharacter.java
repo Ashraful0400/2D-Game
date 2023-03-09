47a9cef48ed66fd3c195e8c6ece3f15567a68a0f
mab28 <mab28@sfu.ca>
2023-03-09 21:43:11 +0000

    Replace MovableCharacter.java

--- game/src/main/java/game276/MovableCharacter.java
+++ game/src/main/java/game276/MovableCharacter.java
@@ -1,21 +1,39 @@
 package game276;
 
+import java.awt.*;
+
 public abstract class MovableCharacter extends StageGameObject {
-    public int speed; // How much character moves each frame; different for each subcalss
-    public GamePanel gp;
+    public int speed; // How much character moves each frame
+    GamePanel gp;
+    public String name;
     // Needed for moveBack()
     public int prevX;
     public int prevY;
+    int hasCheese = 0;
     // CollisionHandler
+public Rectangle solidArea;
+//need to add this below line after implementing at video 8 at 18.21 min
+    //pickUpObject(objIndex);
+public int solidAreaDefaultX, solidAreaDefaultY;
+//check tile collision
 
+public boolean CollisionOn = false;
+//check object collision
+   int objIndex = gp.cChecker.checkObject(this, true);
 
-    MovableCharacter(GamePanel gp, int startingX, int startingY) {
-        this.gp = gp;
-        // Default position
-        this.x = startingX;
-        this.y = startingY;
+    public MovableCharacter(GamePanel gp) {
+        super();
     }
+public void pickUpObject(int i){
+        if(i != 999){
 
+            String objectName = gp.obj[i].name;
+            switch(objectName) {
+                case "exit":
+                    //need to implement game over
+            }
+        }
+}
     public void move() {};
 
     public void moveBack() {
