efda6ba1ece8c9739d0d5bfc28fb6c73230be2ee
mab28 <mab28@sfu.ca>
2023-03-09 20:36:22 +0000

    Upload New File

---
+++ game/src/aa/App.java
@@ -0,0 +1,15 @@
+package game276;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args ) {
+        GameWindow tmp = new GameWindow();
+        tmp.runGame();
+        System.out.println( "Hello World!" );
+    }
+}
