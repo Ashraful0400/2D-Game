cb464ff4559e81348c8b8f4d4a272703e1353633
mab28 <mab28@sfu.ca>
2023-03-09 20:39:14 +0000

    Upload New File

---
+++ game/src/aa/StageGameObject.java
@@ -0,0 +1,10 @@
+package game276;
+
+public abstract class StageGameObject {
+    public int x;
+    public int y; //y increase -> move downward
+    // public Image sprite;
+    // void reactToCollision()
+    public int hitboxLength;
+    public void repaint() {};
+}
