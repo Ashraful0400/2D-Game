0a6906e90cb66459386d1d487dd457ee6c791ae3
mab28 <mab28@sfu.ca>
2023-03-12 13:26:40 -0700

    Added time too

--- game/src/main/java/game276/UI.java
+++ game/src/main/java/game276/UI.java
@@ -2,11 +2,14 @@ package game276;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.text.DecimalFormat;
 
 public class UI {
 
     GamePanel gp;
     Font arial_40;
+    double playTime;
+    DecimalFormat dFormat = new DecimalFormat("#0.00");
    // BufferedImage cheeseImage;//if we use cheese image on screen
     public UI(GamePanel gp){
         this.gp = gp;
@@ -20,5 +23,9 @@ public void draw(Graphics2D g2){
     g2.setColor(Color.white);
     //Change the number to draw in the screen as needed
     g2.drawString("cheese = " + gp.points,25,50);
+
+    //Time
+    playTime += (double)1/60;
+    g2.drawString("Time:" + dFormat.format(playTime),gp.tileSize*11, 65);
     }
 }
