134f8d0a05497474369273e3eb8bfc0ce2ded332
mab28 <mab28@sfu.ca>
2023-03-12 13:05:40 -0700

    New one

--- game/src/main/java/game276/UI.java
+++ game/src/main/java/game276/UI.java
@@ -1,2 +1,24 @@
-package game276;public class UI {
+package game276;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class UI {
+
+    GamePanel gp;
+    Font arial_40;
+   // BufferedImage cheeseImage;//if we use cheese image on screen
+    public UI(GamePanel gp){
+        this.gp = gp;
+
+        arial_40 =  new Font("Jokerman",Font.PLAIN,40);
+
+    }
+
+public void draw(Graphics2D g2){
+    g2.setFont(arial_40);
+    g2.setColor(Color.white);
+    //Change the number to draw in the screen as needed
+    g2.drawString("cheese = " + gp.points,25,50);
+}
 }
